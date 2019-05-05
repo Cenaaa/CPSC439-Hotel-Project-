734a637d2f138fe7b763de765cd3a3004c55c7a5
Sina Heydari <36551361+Cenaaa@users.noreply.github.com>
2019-05-05 15:11:48 -0700

    Update Cleaners.h

--- CPSC 462/Cleaners.h
+++ CPSC 462/Cleaners.h
@@ -9,18 +9,18 @@ class Cleaners : public Services
 {
 private:
 
-    queue<Cleaners> Allcleaners;
+    queue<Cleaners> AllCleaners;
 
 public:
    void addCleaner(Cleaners staff)
    {
        cout << "/n you have been hired as a cleaner" << staff.fname << " " << staff.lname;
-       Allcleaners.push(staff);
+       AllCleaners.push(staff);
    }
    void removeCleaner(Cleaners staff)
    {
        cout << "/n you have been terminated." << staff.fname << " " << staff.lname;
-       Allcleaners.poop(staff);
+       AllCleaners.pop(staff);
    }
 
 };
