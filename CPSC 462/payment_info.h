aa446f6cedcaa9c2796c89af0ff42ce58dcfd0f3
agumabong <32043546+agumabong@users.noreply.github.com>
2019-05-05 03:43:09 -0700

    Update payment_info.h

--- CPSC 462/payment_info.h
+++ CPSC 462/payment_info.h
@@ -5,7 +5,7 @@
 class payment_info
 {
 public:
-	payment_info(std::string guest_name_, std::string bill_number_, std::string cc_ , int date_, int cw2_)
+	payment_info(std::string guest_name_, int bill_number_, int cc_ , int date_, int cw2_)
 	{
 		guest_name = guest_name_;
 		bill_number = bill_number_;
@@ -15,8 +15,8 @@ public:
 	}
 	
 private:
-	std::string guest_name, bill_number, cc;
-	int date,cw2;
+	std::string guest_name;
+	int date,cw2, bill_number, cc;
 };
 
 
