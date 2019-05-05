9376463fce5e1f11881158bd040d73a3d8826bfb
agumabong <32043546+agumabong@users.noreply.github.com>
2019-05-05 03:43:27 -0700

    Update room.h

--- CPSC 462/room.h
+++ CPSC 462/room.h
@@ -1,8 +1,86 @@
-#ifndef room
-#define room
+#ifndef r
+#define r
 
 class room
 {
-	
-}
-#endif room
+	public:
+		room(int room_id_, int people_, int price_)
+		{
+			start_date = 0;
+			end_date = 0;
+			price = price_;
+			room_id = room_id_;
+			people = people_;
+			bool reserved = false;
+		}
+
+		room(std::string guest_name_, int room_id_, int start_date_, int end_date_, int people_, int price_)
+		{
+			price = price_;
+			people = people_;
+			room_id = room_id_;
+			guest_name = guest_name_;
+			start_date = start_date_;
+			end_date = end_date_;
+			bool reserved = true;
+		}
+
+		bool is_clean()
+		{
+			return clean;
+		}
+
+		void set_clean(bool c)
+		{
+			clean = c;
+		}
+
+		bool is_reserved()
+		{
+			return reserved;
+		}
+
+		void set_res(bool res)
+		{
+			reserved = res;
+		}
+
+		int get_id()
+		{
+			return room_id;
+		}
+
+		int get_start()
+		{
+			return start_date;
+		}
+
+		int get_end()
+		{
+			return end_date;
+		}
+
+		std::string get_name()
+		{
+			return guest_name;
+		}
+
+		int get_people()
+		{
+			return people;
+		}
+
+		int get_price()
+		{
+			return price;
+		}
+
+	private:
+		int room_id, start_date, end_date, people, price;
+		std::string guest_name;
+		bool clean, reserved;
+
+
+};
+
+#endif
