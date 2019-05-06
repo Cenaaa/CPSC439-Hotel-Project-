acb7468e9e984f3279615001aa73d9f3770b7ab0
Sina Heydari <36551361+Cenaaa@users.noreply.github.com>
2019-05-05 18:24:36 -0700

    Update Services.h

--- CPSC 462/Services.h
+++ CPSC 462/Services.h
@@ -1,35 +1,81 @@
 #include <iostream>
 #include "room.h"
-#include "cleaners.h"
 using namespace std;
 
 
+struct staff
+{
+    string fname;
+    string lname;
+    staff(string fname, string lname){
+      this.fname = fname;
+      this.lname = lname;
+    }
+};
 
-class Services : public HouseKeeping
+
+
+class Services
 {
 private:
     vector<room> notCleanedRoomvector;
     vector<staff> Allcleaners;
+    HouseKeeping obj;
 
 public:
     void clean()
     {
-        if (notCleanedRoomvector.size()>1)
+        notCleanedRoomvector = obj.retNeedToBeCleanedVector();
+        if (notCleanedRoomvector.size() > 1)
         {
             staff new_staff = getCleaner();
             sendCleaner(new_staff);
         }
-
-
     }
+
+    void addCleaner(staff new_staff)
+    {
+        cout << "/n you have been hired as a cleaner" << new_staff.fname << " " << new_staff.lname << "/n/n";
+        Allcleaners.push_back(new_staff);
+    }
+
+    void removeCleaner(staff ex_staff)
+    {
+        bool deleteit;
+        for (int i = 0 ; i < AllCleaners.size(); i++ )
+        {
+            if (AllCleaners[i].fname == ex_staff.fname &&  AllCleaners[i].lname == ex_staff.lname )
+            {
+                deleteit = false;
+                cout << "/n you have been terminated." << ex_staff.fname << " " << ex_staff.lname;
+                 deleteit = true;
+
+            }
+            if (deleteit)
+            {
+               AllCleaners[i] = AllCleaners[i+1];
+            }
+
+
+        }
+     AllCleaners.pop_back();
+    }
+
 
     staff getCleaner()
     {
-        staff readycleaner = Allcleaners.pop_back();
-        return readycleaner;
+        staff readyCleaner = Allcleaners.pop_back();
+        return readyCleaner;
     }
     void sendCleaner(staff ready_staff)
     {
         cout << "/n/n" << ready_staff.fName << " " << ready_staff.lName << "will be sent to clean your room shortly./n/n";
     }
-}
+
+
+
+
+
+
+
+};
