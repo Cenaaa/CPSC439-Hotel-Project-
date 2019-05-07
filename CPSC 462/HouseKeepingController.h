598dafc7e0d63d47c6d4b8bd05e5613e5781806c
Sina Heydari <36551361+Cenaaa@users.noreply.github.com>
2019-05-07 13:21:47 -0700

    Update HouseKeepingController.h

--- CPSC 462/HouseKeepingController.h
+++ CPSC 462/HouseKeepingController.h
@@ -5,39 +5,47 @@
 #include "room.h"
 
 
-
-
 class HouseKeepingController
 {
 private:
-HouseKeeping myHouseKeeping;
-Services     myService;
+    HouseKeeping myHouseKeeping;
+    Services myService;
 
 public:
+    void cleanARoom(room newRoom, int roomNum)
+    {
+        myHouseKeeping.request_To_Be_Cleaned(newRoom);
+        bool hasBeenOrganazied = myService.clean(roomNum);
+        if (hasBeenOrganazied)
+        {
+            myHouseKeeping.HasBeenCleaned();
+            cout << endl << "The room " << roomNum << " has been scheduled to be cleanned." << endl;
+        }
+        else
+        {
+            cout << endl
+                 << "We do not have enough staff to clean any rooms at the moment (sorry!)."
+                 << endl;
+        }
+    }
 
+    void cancelOrder()
+    {
 
-   void cleanARoom (room newRoom, int index)
-   {
-     myHouseKeeping.request_To_Be_Cleaned(newRoom);
-     bool hasBeenOrganazied = myService.clean(index);
-     if (hasBeenOrganazied){
-       myHouseKeeping.HasBeenCleaned();
-       cout  << endl << "The room is cleaned."<< endl ;
-   }else{
-       cout  << endl << "We do not have enough staff to clean any rooms at the moment (sorry!)."<< endl ;
-   }
-   }
+        myHouseKeeping.HasBeenCleaned();
+        // cout  << endl << "Your order to clean "<< roomNum  << " has been scheduled to be
+        // cleanned."<< endl ;
+    }
 
-   void addStaff(string fnmae,string lname)
-   {
-       staff new_Staff(fnmae,lname);
-       myService.addCleaner(new_Staff);
-   }
+    void addStaff(string fnmae, string lname)
+    {
+        staff new_Staff(fnmae, lname);
+        myService.addCleaner(new_Staff);
+    }
 
-   void removeStaff(string fnmae,string lname)
-   {
-       staff ex_Staff(fnmae,lname);
-       myService.removeCleaner(ex_Staff);
-   }
-
+    void removeStaff(string fnmae, string lname)
+    {
+        staff ex_Staff(fnmae, lname);
+        myService.removeCleaner(ex_Staff);
+    }
 };
